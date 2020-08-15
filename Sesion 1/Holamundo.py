b2f254a7bb9b442da95a5ad475346ab6a6139550
Enrique-99 <enriquealmaraz2010@hotmail.com>
2020-08-15 10:46:02 -0500

    Nuevo

---
+++ Sesion 1/Holamundo.py
@@ -0,0 +1,5 @@
+print ("hola mundo") #holamundo
+print ("hola", "mundo") # "hola mundo"
+
+print ("Hola" + "Mundo") # "Hola Mundo"
+
---
+++ Sesion 1/Solicituddedatos.py
@@ -0,0 +1,5 @@
+numero_entero= int (input("Ingresa un numero entero: "))
+numero_decimal= float (input("Ingresa un numero decimal: "))
+Nombre= input("captura tu nombre:")
+suma= numero_entero + numero_decimal
+print("hola", Nombre , ", el resultado de la suma entre" , str(numero_entero) , "y" , str(numero_decimal) , "es" , str(suma) )
