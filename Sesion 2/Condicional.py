ccee8cf8d7f3f10efa332042c6921aa391251cf5
Enrique-99 <enriquealmaraz2010@hotmail.com>
2020-08-15 11:04:06 -0500

    nuevo

---
+++ Sesion 2/Condicional.py
@@ -0,0 +1,14 @@
+q = int(input("captura un numero Q: "))
+
+h = int(input("captura un numero H: "))
+
+if q < h :
+    print ("q es menor que h")
+
+elif q == h:
+
+    print ("q es igual a h")
+
+else:
+
+    print("h es mayor que q" )
