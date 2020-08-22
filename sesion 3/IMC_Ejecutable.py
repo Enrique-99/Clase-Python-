0ee32b0a1089ae8deb39ecf75233a194e0b1509f
Enrique-99 <enriquealmaraz2010@hotmail.com>
2020-08-22 14:11:44 -0500

    IMC

--- sesion 3/IMC_Ejecutable.py
+++ sesion 3/IMC_Ejecutable.py
@@ -1,1 +1,21 @@
-from Clases imc 
+from class_person_module import Persona
+
+while True:
+
+    print("---------- MENU ----------")
+    print("1.- Registrar Persona")
+    print("2.- Consultar Personas")
+    print("3.- Salir")
+    opcion = input("Elige una opción:")
+    
+    if opcion == "1":
+        p = Persona()
+        p.capturar_informacion()
+        p.genera_DNI()
+        print("---------- Información Persona ----------")
+        print("Peso:" + str(p.calcular_IMC()))
+        print("Mayor de Edad:" + str(p.es_mayor_edad()))
+        p.imprimir_informacion()
+     
+    else:
+        break
---
+++ sesion 3/class_person_module.py
@@ -0,0 +1,46 @@
+import random
+
+class Persona:
+    def __init__(self, nombre="", edad=0, DNI=0, sexo="", peso=0.0, altura=0.0):
+        self.nombre = nombre        
+        self.edad = edad
+        self.DNI = DNI
+        self.sexo = sexo
+        self.peso = peso
+        self.altura = altura
+
+    def calcular_IMC(self):
+        IMC = self.peso / self.altura ** 2
+        if IMC <19:
+            return "Anorexia"
+        elif IMC>= 19 and IMC <25:
+            return "Normal"
+        else:
+            return "Sobrepeso"
+
+    def es_mayor_edad(self):
+        return True if self.edad>=18 else False
+
+    def comprobar_sexo(self):
+        if self.sexo.upper() != "H" and self.sexo.upper() != "M":
+            self.sexo = "H"
+    
+    def imprimir_informacion(self):
+        print("Nombre:" + self.nombre)
+        print("Edad:" + str(self.edad))
+        print("DNI:" + str(self.DNI))
+        print("Sexo:" + self.sexo)
+        print("Peso:" + str(self.peso))
+        print("Altura:" + str(self.altura))
+
+    def genera_DNI(self):
+        self.DNI = random.randint(10000000, 99999999)
+
+    def capturar_informacion(self):
+        self.nombre = input("Captura tu nombre:")
+        self.edad = int(input("Captura tu edad:"))
+        self.sexo = input("Captura tu sexo:")
+        self.peso = float(input("Captura tu peso:"))
+        self.altura = float(input("Captura tu altura:"))
+    
+    
